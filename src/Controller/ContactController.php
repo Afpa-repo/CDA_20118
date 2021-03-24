519765e8209a23b16a6e89eba5a3f17e6a99830a
Kevin-dev-afpa <kequintin@laposte.net>
2021-03-24 13:56:48 +0100

    Amélioration de la fonction contact avec interception sur mailtrap.io

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -7,16 +7,21 @@ use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\DependencyInjection\Container;
+
 
 class ContactController extends AbstractController
 {
     /**
      * @Route("/contact", name="contact")
      */
-    public function index(CartService $cartService, Request $request, \Swift_Mailer $mailer)
+    public function index(CartService $cartService, Request $request, MailerInterface $mailer)
     {
+
         $size = $cartService->getSize();
-        
+
         $form = $this->createForm(ContactType::class);
         $form->handleRequest($request);
 
@@ -24,28 +29,26 @@ class ContactController extends AbstractController
             $contact = $form->getData();
 
             // Ici nous enverrons l'e-mail
-            $message = (new \Swift_Message('Nouveau contact'))
+            $message = (new Email())
 
             // On attribue l'expéditeur
-            ->setFrom($contact['email'])
+            ->From($contact['email'])
 
             // On attribue le destinataire
-            ->setTo('votre@adresse.fr')
+            ->To('adressebidon@hotmail.fr')
 
             // On crée le texte avec la vue
-            ->setBody(
-                $this->renderView(
-                    'emails/contact.html.twig', compact('contact')
-                ),
-                'text/html'
-            )
+
+            ->subject('Welcome to the Space Bar!')
+            ->text("Nice to meet you ! ❤️");
             ;
 
             // On envoie le message
             $mailer->send($message);
 
-            $this->addFlash('message', 'Votre message a été transmis, nous vous répondrons dans les meilleurs délais.'); // Permet un message flash de renvoi
+            $this->addFlash('success', 'Votre message a été transmis, nous vous répondrons dans les meilleurs délais.'); // Permet un message flash de renvoi
         }
+        
         return $this->render('contact/index.html.twig',[
             'contactForm' => $form->createView(),
             'size' => $size
